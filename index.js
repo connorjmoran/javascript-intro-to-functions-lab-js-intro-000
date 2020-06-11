b4c4b8ab3c0c9dfe0a4bd2655d4a5bca64250ee9
Connor Moran <cmoran11@fordham.edu>
2020-06-11 21:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if string.toLowerCase() === string
     return `I can't hear you!`
-  if string.toUpperCase() === string
+  else if string.toUpperCase() === string
     return `YES INDEED!`
-  if else
+  else
     return `I love you, too.`
 }
