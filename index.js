972b72c075c0eab07f589337931607749cad9867
Connor Moran <cmoran11@fordham.edu>
2020-06-11 21:24:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-/* function sayHiToGrandma(string) {
+function sayHiToGrandma(string) {
   switch(true) {
   case string.toLowerCase() === string:
   return `I can't hear you!`;
@@ -22,13 +22,13 @@ function logWhisper(string) {
   return `YES INDEED!`;
   default:
   return `I love you, too.`;};
-} */
+} 
 
-function sayHiToGrandma(string) {
+/* function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
     return `I can't hear you!`
   else if (string.toUpperCase() === string)
     return `YES INDEED!`
   else
     return `I love you, too.`
-}
+} */
